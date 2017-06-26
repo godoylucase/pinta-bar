e35e3bc6f390db4c19e71725803d2201215a7f34
Lucas Godoy <godoy.lucas.e@gmail.com>
2017-06-26 17:12:38 +1200

    Netflix OOS Eureka connecting pintabar with review-center microservice

--- pintabar-review-center/src/main/java/com/pintabar/controllers/DummyCommentRestController.java
+++ pintabar-review-center/src/main/java/com/pintabar/controllers/DummyCommentRestController.java
@@ -1,6 +1,9 @@
 package com.pintabar.controllers;
 
 import com.pintabar.dto.CommentDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +15,13 @@ import java.util.List;
  * Created by lucasgodoy on 26/06/17.
  */
 @RestController
-@RequestMapping("/review-api")
+@RequestMapping(value = "/review-api",
+		consumes = MediaType.APPLICATION_JSON_VALUE,
+		produces = MediaType.APPLICATION_JSON_VALUE)
 public class DummyCommentRestController {
 
 	@RequestMapping(value = "/dummy", method = RequestMethod.GET)
-	public List<CommentDTO> getDummyComments() {
+	public ResponseEntity<List<CommentDTO>> getDummyComments() {
 		List<CommentDTO> comments = new ArrayList<>();
 		comments.add(CommentDTO.builder()
 				.businessUuid("5a0ea89d-2166-4ff8-b3ae-f96c1398a866")
@@ -33,6 +38,6 @@ public class DummyCommentRestController {
 				.userUuid("2110b06a-ae41-449f-85d0-2485d9d8befa")
 				.comment("Good service")
 				.build());
-		return comments;
+		return new ResponseEntity<>(comments, HttpStatus.OK);
 	}
 }
